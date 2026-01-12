873c98933b19b74250ca86a9e93e29443fb00904
Devansh Gaur <gaurdevansh8@gmail.com>
2026-01-12 11:06:35 -0500

    Changed to relative paths

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // <--- Changed to capital 'P' to match your repo
+  base: './', // <--- The DOT (.) is the fix. It makes paths relative.
 })
