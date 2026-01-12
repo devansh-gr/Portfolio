06bdc6ab3717f5e30b457bf5852f1230bb4d997f
Devansh Gaur <gaurdevansh8@gmail.com>
2026-01-12 05:26:42 -0500

    Fixed repo URL and username

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // <--- IMPORTANT: This must match your GitHub repo name exactly
+  base: '/Portfolio/', // <--- Changed to capital 'P' to match your repo
 })
